0b2dde673573b7781429fd06a27855507ebc6398
Ajaykumar Rajasekaran <ajaykumarrajasekaran@Ajaykumars-MacBook-Pro.local>
2023-10-31 22:05:00 +0530

    completion of the experience part

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import Header from "./components/Header";
 import Body from "./components/Body";
-
+import Experience from "./components/Experience";
 export default function Home() {
   return (
     <main className="h-screen overflow-y-scroll bg-gradient-to-b from-slate-800 to-slate-600">
       <Header />
       <Body />
+      <Experience />
     </main>
   );
 }
