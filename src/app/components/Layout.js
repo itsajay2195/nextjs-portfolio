6239ddff566a9bfe68892079fb1ee59bbe102894
Ajaykumar Rajasekaran <ajaykumarrajasekaran@Ajaykumars-MacBook-Pro.local>
2023-11-08 01:03:11 +0530

    added google analytic

---
+++ src/app/components/Layout.js
@@ -0,0 +1,18 @@
+// components/Layout.js
+"use client";
+import { useEffect } from "react";
+import { initGA, logPageView } from ".././../utils/analytics";
+
+const Layout = ({ children }) => {
+  useEffect(() => {
+    if (!window.GA_INITIALIZED) {
+      initGA();
+      window.GA_INITIALIZED = true;
+    }
+    logPageView();
+  }, []);
+
+  return <div>{children}</div>;
+};
+
+export default Layout;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,17 @@ import Body from "./components/Body";
 import Experience from "./components/Experience";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
+import Layout from "./components/Layout";
 export default function Home() {
   return (
     <main className="h-screen overflow-y-scroll bg-gradient-to-b from-slate-800 to-slate-600">
-      <Header />
-      <Body />
-      <Experience />
-      <Skills />
-      <Contact />
+      <Layout>
+        <Header />
+        <Body />
+        <Experience />
+        <Skills />
+        <Contact />
+      </Layout>
     </main>
   );
 }
