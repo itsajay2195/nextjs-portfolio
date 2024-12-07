0895505f844c891e18c2438efb047d67a8ace44f
Ajaykumar Rajasekaran <ajaykumarrajasekaran@Ajaykumars-MacBook-Pro-2.local>
2024-12-07 15:02:28 +0530

    added keywords for seo'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,35 @@ import Contact from "./components/Contact";
 import Layout from "./components/Layout";
 import ParticleArea from "../particleArea/ ParticleArea";
 import { sections, socialIcons } from "../utils/constants";
+import Head from "next/head";
 export default function Home() {
   return (
     <main className="h-screen overflow-y-scroll bg-gradient-to-b from-slate-800 to-slate-600">
+      <Head>
+        <title>Ajaykumar Rajasekaran</title>
+        <meta
+          name="description"
+          content="Ajaykumar Rajasekaran's personal website showcasing my React and React Native projects, blog posts, and portfolio."
+        />
+        <meta
+          name="keywords"
+          content="Ajaykumar Rajasekaran, React developer, React Native, software developer, portfolio, javascript, js, rn, promises, async await, flatlist, optimization, react native performance"
+        />
+        <meta
+          property="og:title"
+          content="Ajaykumar Rajasekaran - React Developer"
+        />
+        <meta
+          property="og:description"
+          content="Explore my personal website where I showcase my projects, blog, and work as a React Native developer."
+        />
+        <meta property="og:image" content="/path-to-image.jpg" />
+        <meta
+          property="og:url"
+          content="https://www.ajaykumarrajasekaran.com"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
       <Layout>
         {/* <ParticleArea /> */}
         <Header sections={sections} socialIcons={socialIcons} />
